37a42b3806c932e2ad6594ada6708fe82b7e26aa
Frankie <info@newcityvapes.com>
2025-02-16 20:48:17 -0500

    change 2/16 test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 import Link from "next/link";
+import Head from "next/head";
 
 export default function HomePage() {
   return (
     <div>
+      <Head>
+        <title>NCV Compare Tool</title>
+        <link rel="icon" href="/NCV_favicon_light.png" />
+      </Head>
       <h1>Welcome to My Compare tool App</h1>
       <Link href="/shopify-page">
         <button>Products</button>
