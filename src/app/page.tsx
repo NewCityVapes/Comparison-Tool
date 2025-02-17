185b7384b61ffdf1512bbb4d171ef90ae37e564b
Frankie <info@newcityvapes.com>
2025-02-16 20:34:13 -0500

    change 2/16 test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function HomePage() {
   return (
     <div>
-      <h1>Welcome to My Shopify App</h1>
+      <h1>Welcome to My Compare tool App</h1>
       <Link href="/shopify-page">
         <button>Products</button>
       </Link>
